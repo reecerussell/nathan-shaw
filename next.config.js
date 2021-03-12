9304ec540b62b614f5d98754ad0f2c6f189b7a4e
Reece Russell <me@reece-russell.co.uk>
2021-03-12 21:53:52 +0000

    deleted uneeded files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  poweredByHeader: false,
-  distDir: 'dist',
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'scss/styles.scss')],
-  },
-};
